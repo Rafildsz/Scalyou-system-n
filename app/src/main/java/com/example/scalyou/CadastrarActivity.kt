6b540e9d43e8bfb05f7d6c520180236f88af742b
Rafael <rodrigues14rafa@gmail.com>
2025-08-27 18:14:51 -0300

    Inserindo ajustes do cadastro

--- app/src/main/java/com/example/scalyou/CadastrarActivity.kt
+++ app/src/main/java/com/example/scalyou/CadastrarActivity.kt
@@ -1,6 +1,7 @@
 package com.example.scalyou
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,18 @@ class CadastrarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastrar)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scrollCadastro)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Botão cancelar
+        val btnCancelar = findViewById<Button>(R.id.btnCancelar)
+        btnCancelar.setOnClickListener {
+            // Fecha a tela atual e volta para o login
+            finish()
         }
     }
 }
